0a7dd660725471061af970b0be5adf780c6c0beb
ParrotCode101 <79524299+ParrotCode101@users.noreply.github.com>
2021-03-02 10:32:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+var userName = prompt("ENTER USERNAME>>")
+var enterAge = prompt("ENTER AGE>>")
+var userGreeting = console.log("Welcome, " + userName + "! This is a fun quiz to see how well you know your Bengal Tiger...")
