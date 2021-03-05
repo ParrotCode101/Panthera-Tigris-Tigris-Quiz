a2034834f226bb0c7cabeb9369dcc815fdd22d09
ParrotCode101 <79524299+ParrotCode101@users.noreply.github.com>
2021-03-05 13:43:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var userName = prompt("ENTER USERNAME>>")
 var enterAge = prompt("ENTER AGE>>")
 var userGreeting = console.log("Welcome, " + userName + "! This is a fun quiz to see how well you know your Bengal Tiger...")
+// Starting the actual game
+var q1 = prompt("Okay, first question: Is the Bengal Tiger the biggest sub-species of Tiger? - y/n")
+
+if q1 == "n" {
+  console.log("WOW!")
+}
+if q1 == "y" {
+  console.log("Er...nice try.")
+}
+
+var q2 = prompt("India is home to 65% of all Tigers. -  true/false")
+if q2 == "false" {
+  console.log("Correct!")
+}
+if q2 == "true" {
+  console.log("...")
+}
+
+// More questions coming!
